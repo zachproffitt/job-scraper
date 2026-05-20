4c930c1b1285bba524fae9316bee915270e03a4b
Zach Proffitt <zachproffitt@hotmail.com>
2026-05-19 20:18:01 -0600

    Restore hot pink New badge on company page

--- pipeline/badges.py
+++ pipeline/badges.py
@@ -1,7 +1,7 @@
 SKILL_COLOR = "3B82F6"
 REMOTE_BADGE = "`Remote`"
 HYBRID_BADGE = "`Hybrid`"
-NEW_BADGE = "`New`"
+NEW_BADGE = '<img src="https://img.shields.io/badge/New-FF69B4?style=flat-square" alt="New">'
 
 
 def skill_badge(skill: str) -> str:
